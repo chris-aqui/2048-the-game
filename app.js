99bdf8fd1d3a6bbf53d905d3de8a76a4a57eec14
christine-aqui <aqui.christine@gmail.com>
2021-03-04 00:15:25 -0500

    wip: adding movement

--- app.js
+++ app.js
@@ -35,3 +35,97 @@ function generate(arr) {
 		// checkForGameOver()
 	} else generate()
 }
+
+//  swipe right
+function moveRight(){
+	for (let i = 0; i < 16; i++) {
+		// find row
+		if(i % 4 === 0){
+			let totalOne = squares[i].innerHTML
+			let totalTwo = squares[i+1].innerHTML
+			let totalThree = squares[i+2].innerHTML
+			let totalFour = squares[i+3].innerHTML
+			// use parseInt to change innerhtml string to a Number
+			let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+			console.log('##row ', row)
+			//
+
+			// filter ot all the numbers from the row?
+			let filteredRow = row.filter(num => num)
+			let missing = 4 - filteredRow.length
+			let zeros = Array(missing).fill(0)
+			let newRow = filteredRow.concat(zeros)
+			console.log('##newRow ', newRow)
+
+			squares[i].innerHTML = newRow[0]
+			squares[i +1].innerHTML = newRow[1]
+			squares[i +2].innerHTML = newRow[2]
+			squares[i +3].innerHTML = newRow[3]
+		}
+	}
+}
+
+// swipe left
+function moveLeft() {
+	for (let i=0; i < 16; i++) {
+		if (i % 4 === 0) {
+			let totalOne = squares[i].innerHTML
+			let totalTwo = squares[i+1].innerHTML
+			let totalThree = squares[i+2].innerHTML
+			let totalFour = squares[i+3].innerHTML
+			let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+			let filteredRow = row.filter(num => num)
+			let missing = 4 - filteredRow.length
+			let zeros = Array(missing).fill(0)
+			let newRow = filteredRow.concat(zeros)
+
+			squares[i].innerHTML = newRow[0]
+			squares[i +1].innerHTML = newRow[1]
+			squares[i +2].innerHTML = newRow[2]
+			squares[i +3].innerHTML = newRow[3]
+		}
+	}
+}
+
+// swipe up
+function moveUp() {
+	for (let i=0; i < 4; i++) {
+		let totalOne = squares[i].innerHTML
+		let totalTwo = squares[i+width].innerHTML
+		let totalThree = squares[i+(width*2)].innerHTML
+		let totalFour = squares[i+(width*3)].innerHTML
+		let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+		let filteredColumn = column.filter(num => num)
+		let missing = 4 - filteredColumn.length
+		let zeros = Array(missing).fill(0)
+		let newColumn = filteredColumn.concat(zeros)
+
+		squares[i].innerHTML = newColumn[0]
+		squares[i +width].innerHTML = newColumn[1]
+		squares[i+(width*2)].innerHTML = newColumn[2]
+		squares[i+(width*3)].innerHTML = newColumn[3]
+	}
+}
+
+// swipe down
+function moveDown() {
+	for (let i=0; i < 4; i++) {
+		let totalOne = squares[i].innerHTML
+		let totalTwo = squares[i+width].innerHTML
+		let totalThree = squares[i+(width*2)].innerHTML
+		let totalFour = squares[i+(width*3)].innerHTML
+		let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+		let filteredColumn = column.filter(num => num)
+		let missing = 4 - filteredColumn.length
+		let zeros = Array(missing).fill(0)
+		let newColumn = zeros.concat(filteredColumn)
+
+		squares[i].innerHTML = newColumn[0]
+		squares[i +width].innerHTML = newColumn[1]
+		squares[i+(width*2)].innerHTML = newColumn[2]
+		squares[i+(width*3)].innerHTML = newColumn[3]
+	}
+}
