9c8341691bec26ef6a3faaae85965a9a2f8500d0
christine-aqui <aqui.christine@gmail.com>
2021-03-07 14:23:51 -0500

    finished game

--- app.js
+++ app.js
@@ -20,19 +20,20 @@ document.addEventListener('DOMContentLoaded', () => {
 			squares.push(square)
 			// console.log('##squares ', squares)
 		}
-		generate(squares)
-    generate(squares)
+		// generate(squares)
+		generate()
+    generate()
 	}
 	// invoke function
 	createBoard()
 
 
 // generate a new number
-function generate(arr) {
-	let randomNumber = Math.floor(Math.random() * arr.length)
-	if (arr[randomNumber].innerHTML == 0) {
-		arr[randomNumber].innerHTML = 2
-		// checkForGameOver()
+function generate() {
+	let randomNumber = Math.floor(Math.random() * squares.length)
+	if (squares[randomNumber].innerHTML == 0) {
+		squares[randomNumber].innerHTML = 2
+		checkForGameOver()
 	} else generate()
 }
 
@@ -58,7 +59,7 @@ function moveRight(){
 			let zeros = Array(missing).fill(0)
 
 			// adds zeros to the right
-			let newRow = filteredRow.concat(zeros)
+			let newRow = zeros.concat(filteredRow)
 
 			squares[i].innerHTML 		= newRow[0]
 			squares[i +1].innerHTML = newRow[1]
@@ -138,5 +139,126 @@ function moveDown() {
 		squares[i+(width*3)].innerHTML = newColumn[3]
 	}
 }
-});
+//
+function combineRow() {
+	for (let i =0; i < 15; i++) {
+		if (squares[i].innerHTML === squares[i +1].innerHTML) {
+			let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +1].innerHTML)
+			squares[i].innerHTML = combinedTotal
+			squares[i +1].innerHTML = 0
+			score += combinedTotal
+			scoreDisplay.innerHTML = score
+		}
+	}
+	checkForWin()
+}
 
+function combineColumn() {
+	for (let i =0; i < 12; i++) {
+		if (squares[i].innerHTML === squares[i +width].innerHTML) {
+			let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +width].innerHTML)
+			squares[i].innerHTML = combinedTotal
+			squares[i +width].innerHTML = 0
+			score += combinedTotal
+			scoreDisplay.innerHTML = score
+		}
+	}
+	checkForWin()
+}
+
+//assign functions to keyCodes
+function control(e) {
+	if(e.keyCode === 37) {
+		keyLeft()
+	} else if (e.keyCode === 38) {
+		keyUp()
+	} else if (e.keyCode === 39) {
+		keyRight()
+	} else if (e.keyCode === 40) {
+		keyDown()
+	}
+}
+document.addEventListener('keyup', control)
+
+function keyRight() {
+	moveRight()
+	combineRow()
+	moveRight()
+	generate()
+}
+
+function keyLeft() {
+	moveLeft()
+	combineRow()
+	moveLeft()
+	generate()
+}
+
+function keyUp() {
+	moveUp()
+	combineColumn()
+	moveUp()
+	generate()
+}
+
+function keyDown() {
+	moveDown()
+	combineColumn()
+	moveDown()
+	generate()
+}
+
+//check for the number 2048 in the squares to win
+function checkForWin() {
+	for (let i=0; i < squares.length; i++) {
+		if (squares[i].innerHTML == 2048) {
+			resultDisplay.innerHTML = 'You WIN'
+			document.removeEventListener('keyup', control)
+			setTimeout(() => clear(), 3000)
+		}
+	}
+}
+
+//check if there are no zeros on the board to lose
+function checkForGameOver() {
+	let zeros = 0
+	for (let i=0; i < squares.length; i++) {
+		if (squares[i].innerHTML == 0) {
+			zeros++
+		}
+	}
+	if (zeros === 0) {
+		resultDisplay.innerHTML = 'You LOSE'
+		document.removeEventListener('keyup', control)
+		setTimeout(() => clear(), 3000)
+	}
+}
+
+//clear timer
+function clear() {
+	clearInterval(myTimer)
+}
+
+
+//add colours
+function addColours() {
+	for (let i=0; i < squares.length; i++) {
+		if (squares[i].innerHTML == 0) squares[i].style.backgroundColor = '#afa192'
+		else if (squares[i].innerHTML == 2) squares[i].style.backgroundColor = '#eee4da'
+		else if (squares[i].innerHTML  == 4) squares[i].style.backgroundColor = '#ede0c8'
+		else if (squares[i].innerHTML  == 8) squares[i].style.backgroundColor = '#f2b179'
+		else if (squares[i].innerHTML  == 16) squares[i].style.backgroundColor = '#ffcea4'
+		else if (squares[i].innerHTML  == 32) squares[i].style.backgroundColor = '#e8c064'
+		else if (squares[i].innerHTML == 64) squares[i].style.backgroundColor = '#ffab6e'
+		else if (squares[i].innerHTML == 128) squares[i].style.backgroundColor = '#fd9982'
+		else if (squares[i].innerHTML == 256) squares[i].style.backgroundColor = '#ead79c'
+		else if (squares[i].innerHTML == 512) squares[i].style.backgroundColor = '#76daff'
+		else if (squares[i].innerHTML == 1024) squares[i].style.backgroundColor = '#beeaa5'
+		else if (squares[i].innerHTML == 2048) squares[i].style.backgroundColor = '#d7d4f0'
+	}
+}
+addColours()
+
+var myTimer = setInterval(addColours, 50)
+
+});
